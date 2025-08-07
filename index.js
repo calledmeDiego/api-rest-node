45653691a1beaca03d60694d162216ce060f804b
calledmeDiego <diegogabo17@gmail.com>
2025-08-07 15:51:18 -0500

    Add Middleware Not Found

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import express  from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Bienvenidos a mi API REST!!!")
+app.use((req, res, next) => {
+    // res.json({ message: 'API en mantenimiento' });
+    console.log(req.method);
+    next();
 });
+
+app.get("/", (req, res) => {
+    res.json({ 'message': 'Bienvenidos a nuestra API REST'})
+});
+
+import notFound from "./src/middlewares/not-found.js";
+
+app.use(notFound)
 
 const PORT = 3000;
 
