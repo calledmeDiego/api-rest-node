5060a48b948d46b19a91956bb1b671b3a1ad566b
calledmeDiego <diegogabo17@gmail.com>
2025-08-07 17:47:06 -0500

    Model added

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,29 +1,6 @@
-const products = [
-  {
-    id: 1,
-    name: "Camiseta Deportiva",
-    price: 150,
-    categories: ["ropa", "deportes"],
-  },
-  {
-    id: 2,
-    name: "Zapatos Running",
-    price: 1200,
-    categories: ["calzado", "deportes"],
-  },
-  {
-    id: 3,
-    name: "Mochila Deportiva",
-    price: 350,
-    categories: ["mochilas", "deportes"],
-  },
-  {
-    id: 4,
-    name: "Auriculares HD",
-    price: 800,
-    categories: ["tecnología", "audio"],
-  },
-];
+import * as Model from "../models/Product.js";
+
+const products = Model.getDataProducts()
 
 export const getAllProducts = (req, res) => {
   const { category } = req.query;
